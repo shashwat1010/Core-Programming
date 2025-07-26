fe34d860e23c02da2ec61bf56ec7bb611b923d8f
Shashwat Rana <126244043+shashwat1010@users.noreply.github.com>
2025-07-26 23:17:49 +0530

    Create PCMaverage.java

---
+++ Level-1 Program/PCMaverage.java
@@ -0,0 +1,10 @@
+class PCMaverage {
+    public static void main(String[] args) {
+        int physics = 85;
+        int chemistry = 90;
+        int maths = 80;
+        int total = physics + chemistry + maths;
+        double average = total / 3.0;
+        System.out.println("Average marks: " + average);
+    }
+}
