f059f650a00742851cc7aa24b01fa07d067d8a69
Shashwat Rana <126244043+shashwat1010@users.noreply.github.com>
2025-07-27 00:10:41 +0530

    Create BasicCalculator.java

---
+++ Level-2 Program/BasicCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double number1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double number2 = input.nextDouble();
+
+        double sum = number1 + number2;
+        double diff = number1 - number2;
+        double prod = number1 * number2;
+        double div = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " 
+            + number1 + " and " + number2 + " is: ");
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + prod);
+        System.out.println("Division: " + div);
+    }
+}
