7fde24ff89f349a47c993ee34dba25cee3c31a80
Shashwat Rana <126244043+shashwat1010@users.noreply.github.com>
2025-08-10 21:08:29 +0530

    Create Sum of Two Numbers.java

---
+++ Level-2/Sum of Two Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+        System.out.println("Sum = " + (a + b));
+    }
+}
