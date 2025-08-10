175f07954e4a0075f6c0eee3d8a77ded75b8dc7e
Shashwat Rana <126244043+shashwat1010@users.noreply.github.com>
2025-08-10 21:18:02 +0530

    Create Exception handling — divide two numbers.java

---
+++ Level-2/Exception handling — divide two numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+            System.out.println("Result: " + (a / b));
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero not allowed!");
+        }
+    }
+}
